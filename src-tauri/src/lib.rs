4c192c05e636b0ea4ce09495f783abe58fd31b1d
NYG-kali-user <cogito.ergo.sum.amane@gmail.com>
2025-09-15 15:37:11 +0900

    Update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -163,6 +163,12 @@ fn exit_app(app: tauri::AppHandle) {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle()
+                .plugin(tauri_plugin_updater::Builder::new().build())?;
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![
